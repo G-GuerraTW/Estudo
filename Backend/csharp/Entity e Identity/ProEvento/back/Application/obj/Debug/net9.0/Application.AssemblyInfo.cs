cc9037f76d47483442ab8e63b4c09cbfc195afe5
Gabriel Guerra <g.goatguerra.g@gmail.com>
2025-01-10 18:58:36 -0300

    Subindo Contratos de interface do serviço de Evento

--- Backend/csharp/Entity e Identity/ProEvento/back/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Backend/csharp/Entity e Identity/ProEvento/back/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0fd13207a6edf435f78b52e7388f611b36fb0c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52db36dc195b668b8b82350f72923278a24a0c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/csharp/Entity e Identity/ProEvento/back/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Backend/csharp/Entity e Identity/ProEvento/back/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0fd13207a6edf435f78b52e7388f611b36fb0c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52db36dc195b668b8b82350f72923278a24a0c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
