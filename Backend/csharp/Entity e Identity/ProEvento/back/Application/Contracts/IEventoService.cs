d346191a7ba0c38c935a273574dd93fee374a6a7
Gabriel Guerra <g.goatguerra.g@gmail.com>
2025-01-10 18:57:36 -0300

    Subindo Contratos de interface do serviço de Evento

---
+++ Backend/csharp/Entity e Identity/ProEvento/back/Application/Contracts/IEventoService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Contracts
+{
+    public interface IEventoService
+    {
+        public Task<Evento> AddEvento(Evento evento);
+        public Task<Evento> UpdateEvento(Evento evento);
+        public Task<Evento> DeleteEvento(Evento evento);
+        public Task<Evento[]> GetAllEventosAsync(bool IncludePalestrante = false);
+        public Task<Evento[]> GetAllEventosByTemaAsync( string Tema,bool IncludePalestrante = false);
+        public Task<Evento> GetEventoByIdAsync(int Id, bool IncludePalestrante = false);        
+    }
+}
