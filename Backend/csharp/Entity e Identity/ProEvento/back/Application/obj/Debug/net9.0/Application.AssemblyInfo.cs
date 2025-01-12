033e2e016dd8b260fa03c440e7bbec28543bfdab
Gabriel Guerra <g.goatguerra.g@gmail.com>
2025-01-12 16:59:37 -0300

    Estudos Entity & Identity | Configurando FLUENT-API

--- Backend/csharp/Entity e Identity/ProEvento/back/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Backend/csharp/Entity e Identity/ProEvento/back/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9037f76d47483442ab8e63b4c09cbfc195afe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51db9a8ac07d031cc53779a4b1d9d6f9d1cbd8ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
