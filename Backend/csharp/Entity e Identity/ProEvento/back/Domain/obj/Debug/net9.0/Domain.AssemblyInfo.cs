c18d3af1411be3fe0ea52ab2dff8c0cba23420db
Gabriel Guerra <g.goatguerra.g@gmail.com>
2025-01-13 15:48:54 -0300

    ProEventos Entity - Identity

--- Backend/csharp/Entity e Identity/ProEvento/back/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Backend/csharp/Entity e Identity/ProEvento/back/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0fd13207a6edf435f78b52e7388f611b36fb0c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d3b022f9fac8e8894600cce90c63783e32524f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
