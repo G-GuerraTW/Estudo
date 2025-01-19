c7f59f9ef5751c679a49b08e1553a9d758b90bf9
Gabriel Guerra <g.goatguerra.g@gmail.com>
2025-01-18 21:32:23 -0300

    Subindo Controller Palestrante

---
+++ Backend/csharp/Entity e Identity/ProEvento/back/API/Controller/PalestranteController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controller
+{
+    [Controller]
+    [Route("api/[Controller]")]
+    public class PalestranteController : ControllerBase
+    {
+        
+    }
+}
